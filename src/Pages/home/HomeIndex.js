db14ad74c056de2f72017216e7a22fae792ba0d6
Helviley Lacerda <helviley.melo@gmail.com>
2023-01-03 14:14:23 -0300

    E-commerce automotivo

--- src/Pages/home/HomeIndex.js
+++ src/Pages/home/HomeIndex.js
@@ -1,26 +1,42 @@
 import styles from "./HomeIndex.module.css"
+import logo from "../../components/img/Tora_Logo.png"
+
 function Home() {
+  
+  function Login(){
+    
+  }
+
   return (
-    <div className="header">
-      <div>
-        {/* <img className="logo" src="https://o.remove.bg/downloads/c557502e-9765-4746-b9e7-c22086ce34b9/ToraLogo-removebg-preview.png" 
-        alt="Home" />
-      </div>
-      <div className="buttons">
-        <button className="button1">Button 1</button>
-        <button className="button2">Button 2</button> */}
+    <div className="main">
+      <div className="header">
+        <div>
+          <img className="logo" src={logo} 
+          alt="Home" />
+        </div>
+        <div className="buttons">
+          <button className="button1">Button 1</button>
+          <button className="button2" onClick={Login}>Login</button>
+        </div>
       </div>
       <div className={styles.conteiner}>
-        {/* <div className="main">
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-          <img className={styles.image} src="https://via.placeholder.com/100" alt="" />
-        </div> */}
+        <div className="main">
+          <div className="row_no_gutters">
+            <div className="vehicle">
+              {/* <a className="vehicle-inner" rel="noopener" data-cy="#$" href="https://via.placeholder.com"/> */}
+                <div className="car-header">
+                  <img src="https://via.placeholder.com/120" alt="" />
+                </div>
+                <div className="car-body">
+                  <h2 className="car-name">Veículo1</h2>
+                  <p className="car-details">Veículo xx 2012, xx km</p>
+                  <div className="payment-row">
+                    <span className="payment-total">Valor</span>
+                  </div>
+                </div>
+            </div> 
+          </div>
+        </div>
       </div>
 
       {/* <div className="footer"></div> */}

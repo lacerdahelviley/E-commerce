3a178f7b619d35f1b4b706807c189b15adb47ad7
Helviley Lacerda <helviley.melo@gmail.com>
2023-01-13 14:34:25 -0300

    E-commerce automotivo

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,66 +2,64 @@ import style from "./index.module.css";
 import logo from "../img/Tora_Logo.png";
 import loupe from "../img/loupe.png";
 import bag from "../img/Shopping_Bag.png";
-import Logout from "../img/Logout.png"
+import Logout from "../img/Logout.png";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <>
-      <div className="App">
-        <div className={style.header}>
-          <div className={style.HeaderContents}>
-            <ul>
-              <ul>
+      <div className={style.header}>
+        <div className={style.HeaderContents}>
+          <div className={style.HeaderButtons}>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
                 <Link to="/">
                   <img className={style.logo} src={logo} alt="Home"></img>
                 </Link>
-              </ul>
+              </li>
             </ul>
-            <div className={style.HeaderButtons}>
-              <ul>
-                <ul>
-                  <Link to="/">
-                    <button className={style.button}>Página principal</button>
-                  </Link>
-                </ul>
-              </ul>
-              <ul>
-                <ul>
-                  <Link to="/registration_screen/index">
-                    <button className={style.button}>
-                      Cadastro de veículos
-                    </button>
-                  </Link>
-                </ul>
-              </ul>
-              <ul>
-                <ul>
-                  <Link to="/vehicle_sales/index">
-                    <button className={style.button}>Cadastro de vendas</button>
-                  </Link>
-                </ul>
-              </ul>
-              <ul>
-                <ul>
-                  <img className={style.loupe} src={loupe} alt="search" />
-                </ul>
-              </ul>
-              <ul>
-                <ul>
-                  <img
-                    className={style.ShoppingBag}
-                    src={bag}
-                    alt="shopping_bag"
-                  />
-                </ul>
-              </ul>
-              <ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
+                <Link to="/">
+                  <button className={style.button}>Página principal</button>
+                </Link>
+              </li>
+            </ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
+                <Link to="/registration_screen/index">
+                  <button className={style.button}>Cadastro de veículos</button>
+                </Link>
+              </li>
+            </ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
+                <Link to="/vehicle_sales/index">
+                  <button className={style.button}>Cadastro de vendas</button>
+                </Link>
+              </li>
+            </ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
+                <img className={style.loupe} src={loupe} alt="search" />
+              </li>
+            </ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
+                <img
+                  className={style.ShoppingBag}
+                  src={bag}
+                  alt="shopping_bag"
+                />
+              </li>
+            </ul>
+            <ul className={style.HeaderSeparator}>
+              <li className={style.HeaderTopic}>
                 <Link to="/login/Index">
                   <img className={style.Logout} src={Logout} alt="Logout" />
                 </Link>
-              </ul>
-            </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
